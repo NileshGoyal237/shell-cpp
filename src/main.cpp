02efd001eaba53980ec3f347201e0dff0a34d5ee
nilesh goyal <goyalnilesh237@gmail.com>
2026-05-26 01:21:32 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <string>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <vector>
 
 int main() {
   std::cout << std::unitbuf;
@@ -13,31 +15,42 @@ int main() {
   while (true) {
     std::cout << "$ ";
     std::getline(std::cin, line);
+
     std::stringstream ss(line);
     ss >> command;
+
     if (command == "echo") {
       std::string word;
       while (ss >> word)
         std::cout << word << " ";
       std::cout << std::endl;
-    } else if (command == "exit")
+
+    } else if (command == "exit") {
+
       break;
-    else if (command == "type") {
+
+    } else if (command == "type") {
+
       bool found = false;
       std::string builtin[3] = {"echo", "exit", "type"};
       std::string command_to_know;
       ss >> command_to_know;
-      for (int i = 0; i <= builtin->length(); i++)
+
+      for (int i = 0; i < 3; i++) {
         if (builtin[i] == command_to_know) {
           std::cout << command_to_know << " is a shell builtin\n";
           found = true;
         }
+      }
+
       if (!found) {
         std::string path_env = std::getenv("PATH");
         std::stringstream ss_path(path_env);
         std::string path;
+
         while (std::getline(ss_path, path, ':')) {
           std::string full_path = path + '/' + command_to_know;
+
           if (access(full_path.c_str(), X_OK) == 0) {
             std::cout << command_to_know << " is " << full_path << std::endl;
             found = true;
@@ -45,10 +58,39 @@ int main() {
           }
         }
       }
+
       if (!found) {
         std::cout << command_to_know << ": not found\n";
       }
-    } else
-      std::cout << command << ": command not found\n";
+
+    } else {
+
+      std::vector<std::string> args;
+      args.push_back(command);
+
+      std::string arg;
+      while (ss >> arg)
+        args.push_back(arg);
+
+      std::vector<char*> c_args;
+      for (auto& s : args)
+        c_args.push_back(&s[0]);
+
+      c_args.push_back(nullptr);
+
+      pid_t pid = fork();
+
+      if (pid == 0) {
+
+        execvp(command.c_str(), c_args.data());
+
+        std::cout << command << ": command not found\n";
+        exit(1);
+
+      } else {
+
+        waitpid(pid, nullptr, 0);
+      }
+    }
   }
 }
