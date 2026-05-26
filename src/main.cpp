03ec318ab5432ed22b90f1fb1f3779355ad1fc0b
nilesh goyal <goyalnilesh237@gmail.com>
2026-05-26 12:34:12 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -88,7 +88,7 @@ int main() {
       for (int i = 1; i < tokens.size(); i++) {
         std::cout << tokens[i];
 
-        if (i + 1 < tokens.size())
+        if (i + 1 < tokens.size() && tokens[i]!=" ")
           std::cout << " ";
       }
 
