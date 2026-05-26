aa3dc9d334b87aeceeffa3ed56c782ff83940319
nilesh goyal <goyalnilesh237@gmail.com>
2026-05-26 18:43:19 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -320,15 +320,15 @@ int main() {
 
       bool found = false;
 
-      std::string builtin[5] = {
-        "echo", "exit", "type", "pwd", "cd"
+      std::string builtin[6] = {
+        "echo", "exit", "type", "pwd", "cd" ,"complete"
       };
 
       if (tokens.size() > 1) {
 
         std::string command_to_know = tokens[1];
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
 
           if (builtin[i] == command_to_know) {
 
